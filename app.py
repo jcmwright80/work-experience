2094a5665aa3c247235c9f46cccd3d1f4220c4c2
Jim Wright <jim.wright@soprabankimg.com>
2025-06-06 13:17:03 +0100

    initial project

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template_string
+from db import get_connection
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        task = request.form["task"]
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO todos (task) VALUES (%s)", (task,))
+        conn.commit()
+        conn.close()
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, task FROM todos")
+    todos = cur.fetchall()
+    conn.close()
+
+    return render_template_string("""
+        <form method="post">
+            <input name="task"/>
+            <button>Add</button>
+        </form>
+        <ul>
+        {% for id, task in todos %}
+            <li>{{ task }}</li>
+        {% endfor %}
+        </ul>
+    """, todos=todos)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,9 @@
+import MySQLdb
+
+def get_connection():
+    return MySQLdb.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        db="todo"
+    )
